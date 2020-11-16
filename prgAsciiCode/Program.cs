ef2791abef8fa7a7beaf2202f0d043b535832bf5
FANUX04 <gabry04.gf@gmail.com>
2020-11-16 10:57:51 +0100

    AsciiCode: creazione progetto

--- prgAsciiCode/Program.cs
+++ prgAsciiCode/Program.cs
@@ -6,7 +6,7 @@ namespace prgAsciiCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("SONO FANELLA");
         }
     }
 }
