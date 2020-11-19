e5627daf041ebf9d3b9a870df505e8d46e44510d
FANUX04 <gabry04.gf@gmail.com>
2020-11-19 12:16:31 +0100

    cosice ASCII

--- prgAsciiCode/Program.cs
+++ prgAsciiCode/Program.cs
@@ -6,7 +6,18 @@ namespace prgAsciiCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("SONO FANELLA");
+            Console.WriteLine("inserisci un valore numerico o alfanumerico da convertire in ASCII");
+            char carattere = Convert.ToChar(Console.ReadLine());
+
+            int number = carattere;
+            // aggiunge 4 al numero decimale corrispondente della lettera inserita
+            //number = number + 4;
+            
+            Console.WriteLine($"il carattere digitato corrisponde al numero....{number.ToString()}");
+            string binario = Convert.ToString(number, 2).PadLeft(8,'0');
+
+            Console.WriteLine($"il carattere in codice binario corrisponde alla sequenza di bit {binario}");
+            Console.ReadKey();
         }
     }
 }
